ad6f7ebd2479e495a3d4216b92d3614a822e68d3
Joseph Petersen <josephp90@gmail.com>
2017-12-06 00:24:14 +0100

    Gotta love Gradle Kotlin DSL :heart:

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,45 @@
-import org.gradle.kotlin.dsl.jenkinsPlugin
 import org.jenkinsci.gradle.plugins.jpi.JpiDeveloper
 import org.jenkinsci.gradle.plugins.jpi.JpiLicense
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-group = "org.jenkinsci.plugins"
-version = "1.0.0-SNAPSHOT"
-
-repositories {
-    maven(url = "https://repo.jenkins-ci.org/public/")
-    jcenter()
-}
-
-val kotlinVersion by extra { "1.1.60" }
+val ktlintVersion by project
+val jenkinsCoreVersion by project
+val jenkinsCredentialsPluginVersion by project
+val sezpozVersion by project
+val atriumVersion by project
 
 plugins {
-    val kotlinVersion = "1.1.60"
-    kotlin("jvm") version kotlinVersion
-    kotlin("kapt") version kotlinVersion
+    kotlin("jvm") version "1.2.0"
+    kotlin("kapt") version "1.2.0"
     id("org.jenkins-ci.jpi") version "0.24.0"
+    id("org.jetbrains.dokka") version "0.9.15"
+    id("com.diffplug.gradle.spotless") version "3.7.0"
+}
+
+dependencies {
+    compile(kotlin("stdlib-jre8"))
+
+    testCompile("junit:junit:4.12")
+    testCompile("ch.tutteli:atrium-cc-en_UK-robstoll:$atriumVersion")
+    testCompile("ch.tutteli:atrium-verbs:$atriumVersion")
+
+    jenkinsPlugins("org.jenkins-ci.plugins:credentials:$jenkinsCredentialsPluginVersion")
+
+    jenkinsTest("org.jenkins-ci.main:jenkins-test-harness:2.32") { isTransitive = true }
+
+    kapt("net.java.sezpoz:sezpoz:$sezpozVersion")
 }
 
 jenkinsPlugin {
-    coreVersion = "2.73.3"
     displayName = "Accurev Client Plugin"
     shortName = "accurev-client"
+    gitHubUrl = "https://github.com/casz/accurev-client-plugin"
+    url = "https://wiki.jenkins.io/display/JENKINS/Accurev+Client+Plugin"
+
+    coreVersion = jenkinsCoreVersion as String
+    compatibleSinceVersion = coreVersion
+    fileExtension = "jpi"
+    pluginFirstClassLoader = true
     workDir = file("$buildDir/work")
 
     developers = this.Developers().apply {
@@ -31,6 +47,7 @@ jenkinsPlugin {
             setProperty("id", "casz")
             setProperty("name", "Joseph Petersen")
             setProperty("email", "josephp90@gmail.com")
+            setProperty("timezone", "UTC+1")
         })
     }
     licenses = this.Licenses().apply {
@@ -40,27 +57,44 @@ jenkinsPlugin {
     }
 }
 
-dependencies {
-    compile(kotlin("stdlib-jre8", kotlinVersion))
-    compile(kotlin("reflect", kotlinVersion))
-
-    jenkinsPlugins("org.jenkins-ci.plugins:credentials:2.1.16@jar")
+spotless {
+    kotlin {
+        // optionally takes a version
+        ktlint(ktlintVersion as String)
+    }
+    kotlinGradle {
+        target(listOf("*.gradle.kts"))
+        ktlint(ktlintVersion as String)
+    }
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
 
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
 }
 
 task<Wrapper>("wrapper") {
     gradleVersion = "4.3.1"
     distributionType = Wrapper.DistributionType.ALL
+}
+
+repositories {
+    maven(url = "https://repo.jenkins-ci.org/public/")
+    jcenter()
+}
+
+// Workaround for https://issues.jenkins-ci.org/browse/JENKINS-48353
+configurations.all { exclude(module = "junit-dep") }
+
+// Workaround for https://github.com/Kotlin/dokka/issues/146
+buildscript {
+    repositories {
+        maven(url = "https://repo.jenkins-ci.org/public/")
+        jcenter()
+    }
 }
