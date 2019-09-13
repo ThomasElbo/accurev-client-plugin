5e6808a4ee3371606c0bd83349ae1c4d08f0441d
Thomas <tmel@widex.com>
2019-09-13 12:45:30 +0200

    Added serr to test

--- src/test/kotlin/jenkins/plugins/accurevclient/CacheTest.kt
+++ src/test/kotlin/jenkins/plugins/accurevclient/CacheTest.kt
@@ -39,12 +39,13 @@ class CacheTest {
                         username.isNotBlank() &&
                         password.isNotEmpty()
         )
-
+        System.err.println("Creating project")
         val project = rule.createFreeStyleProject()
         val accurev = Accurev.with(TaskListener.NULL, EnvVars())
                 .at(project.buildDir).on(url)
         val client = accurev.client
         client.login().username(username).password(Secret.fromString(password)).execute()
+        System.err.println("Assert logged in")
         assertTrue(client.getInfo().loggedIn)
 
         val create = MkFunctions(client)
@@ -52,6 +53,7 @@ class CacheTest {
         val depot = create.mkDepot()
 
         var streams = client.getStreams(depot)
+        System.err.println("Assert stream added")
         assertEquals(1, streams.list.size)
         assertThat(streams.list, contains(
                 hasProperty("name", equalTo(depot))
@@ -61,11 +63,13 @@ class CacheTest {
         val stream2 = create.mkStream(stream)
 
         streams = client.getStreams(depot)
+        System.err.println("Assert updated cache list of 3 items")
         assertEquals(3, streams.list.size)
         assertThat(streams.list, contains(
                 hasProperty("name", equalTo(depot)),
                 hasProperty("name", equalTo(stream)),
                 hasProperty("name", equalTo(stream2))
         ))
+        System.err.println("Test ended")
     }
 }
