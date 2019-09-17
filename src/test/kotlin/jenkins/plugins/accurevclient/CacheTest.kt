7ce5f8c8971d6a0da7bc2c61465ceab39f7253dc
Thomas <tmel@widex.com>
2019-09-17 08:53:55 +0200

    Forgot an import..

--- src/test/kotlin/jenkins/plugins/accurevclient/CacheTest.kt
+++ src/test/kotlin/jenkins/plugins/accurevclient/CacheTest.kt
@@ -11,6 +11,7 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.contains
 import org.hamcrest.Matchers.equalTo
 import org.hamcrest.Matchers.hasProperty
+import org.junit.Assume
 import org.junit.Rule
 import org.junit.Test
 
