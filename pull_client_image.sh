1bfd7449d04ccfbfe9ebf2e2b0c8e9d583213776
Thomas <tmel@widex.com>
2019-09-20 11:26:26 +0200

    Update pull_client_image.sh

--- pull_client_image.sh
+++ pull_client_image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo "$accurevclientdockerimage" | git clone https://github.com/WiMills/accurev-client-docker.git
+git clone https://$accurevclientdockerimage@github.com/WiMills/accurev-client-docker.git
 docker build -t accurev-client ./accurev-client-docker
